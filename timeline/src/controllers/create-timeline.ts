92a5a0444eed46750820c69722c1170fd4dd6e3b
Avner <avner.conceicao@hotmail.com>
2020-06-21 12:14:01 -0300

    feat: should return a timeline with all events inside

--- timeline/src/controllers/create-timeline.ts
+++ timeline/src/controllers/create-timeline.ts
@@ -1,10 +1,16 @@
 import UserParams from "@src/types/user-params";
-import Timeline from "@src/types/timeline";
+import Timeline, { Event } from "@src/types/timeline";
 import timelineRepo from '@src/ports/repos/timeline'
+
+const createWelcomeEvent = (userParams:UserParams): Event => ({
+  description: `Welcome ${userParams.fullname}!`,
+  datetime: new Date()
+})
 
 export default async (userParams: UserParams): Promise<Timeline> => {
   const timeline: Timeline = {
-    userId: userParams.userId
+    userId: userParams.userId,
+    events: [createWelcomeEvent(userParams)]
   }
 
   await timelineRepo.insert(timeline)
