dec4d8d62db267ec7c36de783a337cf38e5dc16b
Avner <avner.conceicao@hotmail.com>
2020-06-21 11:10:48 -0300

    feat: should persist user in the database

--- bank-account/src/controllers/create-bank-account.ts
+++ bank-account/src/controllers/create-bank-account.ts
@@ -1,11 +1,15 @@
 import User, { UserParams, UserBankAccount } from "@src/types/user";
 import bankPartner from '@src/ports/bank-partner'
+import userRepo from '@src/ports/repo/user'
+import { uuid } from 'uuidv4'
 
 export default async (userParams: UserParams): Promise<UserBankAccount> => {
   const user: User = {
-    id: '',
+    id: uuid(),
     fullname: userParams.fullname
   }
+
+  await userRepo.insert(user)
 
   await bankPartner.createAccount(user)
 
