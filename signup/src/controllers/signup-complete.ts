7377b87a1d9f6ead9245dce5e4a389932015ed9c
Avner <avner.conceicao@hotmail.com>
2020-06-21 10:09:16 -0300

    feat: should send a notification when a signup is complete

--- signup/src/controllers/signup-complete.ts
+++ signup/src/controllers/signup-complete.ts
@@ -1,6 +1,8 @@
 import signupRepo from '@src/ports/repos/signup'
+import singupNotifier from '@src/ports/notifiers/singup'
 
 export default async (token: string): Promise<void> => {
   const signup = await signupRepo.getByToken(token)
   await signupRepo.updateStatus(signup, 'COMPLETE')
+  await singupNotifier.complete(signup)
 }
