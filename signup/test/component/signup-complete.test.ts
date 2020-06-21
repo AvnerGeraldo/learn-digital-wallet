965621218dfb25c362a863e45dcbabeebd955510
Avner <avner.conceicao@hotmail.com>
2020-06-21 10:09:31 -0300

    test: should send a notification when a signup is complete

--- signup/test/component/signup-complete.test.ts
+++ signup/test/component/signup-complete.test.ts
@@ -2,9 +2,12 @@ import { expect } from 'chai'
 import signupComplete from '@src/controllers/signup-complete'
 import { SinonStub, stub, restore } from 'sinon';
 import signupRepo from '@src/ports/repos/signup'
+import signupNotifier from '@src/ports/notifiers/singup'
 import Signup from '@src/types/signup';
 
-let getByTokenSignup: SinonStub, updateStatusSignup: SinonStub
+let getByTokenSignup: SinonStub, 
+updateStatusSignup: SinonStub,
+completeNotificationSignup: SinonStub
 
 const signup: Signup = {
   token: 'some-token',
@@ -20,6 +23,7 @@ describe('SignUp Complete', () => {
   beforeEach(() => {
     getByTokenSignup = stub(signupRepo, "getByToken")
     updateStatusSignup = stub(signupRepo, "updateStatus")
+    completeNotificationSignup = stub(signupNotifier, "complete")
   })
 
   afterEach(() => restore())
@@ -31,5 +35,14 @@ describe('SignUp Complete', () => {
     await signupComplete(token)
 
     expect(updateStatusSignup).to.have.been.calledWith(signup, 'COMPLETE')
+  })
+
+  it('should send a notification when a signup is complete', async () => {
+    const token = 'some-token'
+    getByTokenSignup.resolves(signup)
+
+    await signupComplete(token)
+
+    expect(completeNotificationSignup).to.have.been.calledOnce
   })
 })
