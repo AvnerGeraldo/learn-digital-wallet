709aed4531707b629b583f6d50e575dce53e02f8
Avner <avner.conceicao@hotmail.com>
2020-06-21 11:11:03 -0300

    test: should persist user in the database

--- bank-account/test/component/create-bank-account.test.ts
+++ bank-account/test/component/create-bank-account.test.ts
@@ -1,14 +1,17 @@
 import { expect } from 'chai'
 import { SinonStub, restore, stub } from 'sinon';
 import bankPartner from '@src/ports/bank-partner'
+import userRepo from '@src/ports/repo/user.ts'
 import createBankAccount from '@src/controllers/create-bank-account'
-import { UserParams } from '@src/types/user';
+import User, { UserParams } from '@src/types/user';
 
-let createAccountBankPartner: SinonStub
+let createAccountBankPartner: SinonStub,
+insertUser: SinonStub
 
 describe('Create bank account', () => {
   beforeEach(() => {
     createAccountBankPartner = stub(bankPartner, 'createAccount')
+    insertUser = stub(userRepo, 'insert')
   })
 
   afterEach(() => restore())
@@ -21,5 +24,19 @@ describe('Create bank account', () => {
     await createBankAccount(userParams)
 
     expect(createAccountBankPartner).to.have.been.calledOnce
+  });
+
+  it('should persist user in the database', async () => {
+    const userParams: UserParams = {
+      fullname: 'Some fullname'
+    }
+
+    await createBankAccount(userParams)
+
+    expect(insertUser).to.have.been.calledOnce
+
+    const insertedUser = insertUser.getCall(0).args[0] as User
+    expect(insertedUser.id).to.be.a('string').that.has.length(36)
+    expect(insertedUser.fullname).to.be.equal(userParams.fullname)
   });
 });
